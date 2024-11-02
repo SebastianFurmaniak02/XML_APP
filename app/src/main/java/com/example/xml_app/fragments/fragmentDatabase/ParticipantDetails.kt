9671d3045eaa450db00fac24edec9b2f4e8ee98a
Seba <Seba@DESKTOP-L993LD6>
2024-11-02 14:04:02 +0100

    Participant details fixes.

--- app/src/main/java/com/example/xml_app/fragments/fragmentDatabase/ParticipantDetails.kt
+++ app/src/main/java/com/example/xml_app/fragments/fragmentDatabase/ParticipantDetails.kt
@@ -42,15 +42,15 @@ class ParticipantDetails : AppCompatActivity() {
                 finish()
             }
 
-            findViewById<TextView>(R.id.textViewParticipantFirstName).text = participant.firstName
-            findViewById<TextView>(R.id.textViewParticipantLastName).text = participant.lastName
-            findViewById<TextView>(R.id.textViewParticipantEmail).text = participant.email
-            findViewById<TextView>(R.id.textViewParticipantGender).text = participant.gender
-            findViewById<TextView>(R.id.textViewParticipantStudentStatus).text = when {
+            findViewById<TextView>(R.id.textViewParticipantFirstName).text = "First name: " + participant.firstName
+            findViewById<TextView>(R.id.textViewParticipantLastName).text = "Last name: " + participant.lastName
+            findViewById<TextView>(R.id.textViewParticipantEmail).text = "Email: " + participant.email
+            findViewById<TextView>(R.id.textViewParticipantGender).text = "Gender: " + participant.gender
+            findViewById<TextView>(R.id.textViewParticipantStudentStatus).text = "Student status: " + when {
                 participant.studentStatus == 1 -> "Yes"
                 else -> "No"
             }
-            findViewById<TextView>(R.id.textViewParticipantSkillLevel).text = participant.skillLevel.toString()
+            findViewById<TextView>(R.id.textViewParticipantSkillLevel).text = "Skill level: " + participant.skillLevel.toString()
 
             findViewById<Button>(R.id.buttonDelete).setOnClickListener {
                 deleteParticipant()
